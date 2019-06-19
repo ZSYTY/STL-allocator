e578891a82a02d647ba5571e02503fd08694463d
Darktulip <50730736+Darktulip@users.noreply.github.com>
2019-06-20 00:56:57 +0800

    New start

--- allocator.hpp
+++ allocator.hpp
@@ -8,20 +8,20 @@ template <class T>
 struct Mallocator
 {
     typedef T value_type;
-    
+
     Mallocator() = default;
-    
+
     template <class U>
     constexpr Mallocator(const Mallocator<U> &) noexcept {}
-    
-    [[nodiscard]] T *allocate(std::size_t n) {
+
+    T *allocate(std::size_t n) {
         if (n > std::size_t(-1) / sizeof(T))
             throw std::bad_alloc();
         if (auto p = static_cast<T *>(std::malloc(n * sizeof(T))))
             return p;
         throw std::bad_alloc();
     }
-    
+
     void deallocate(T* p, std::size_t) noexcept {
         std::free(p);
     }
@@ -30,7 +30,7 @@ struct Mallocator
 template <class T, class U>
 bool operator==(const Mallocator<T> &, const Mallocator<U> &) {
     return true;
-} 
+}
 
 template <class T, class U>
 bool operator!=(const Mallocator<T> &, const  Mallocator<U> &) {
