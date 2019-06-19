9ad67cf3679322c1c234efb10478d683edddd3b1
darktulip <darktulip@126.com>
2019-06-20 01:12:18 +0800

    refactor: sync with remote repo

--- allocator.hpp
+++ allocator.hpp
@@ -7,34 +7,37 @@
 template <class T>
 struct Mallocator
 {
-    typedef T value_type;
-
+   	typedef T value_type;
+    
     Mallocator() = default;
-
+    
     template <class U>
-    constexpr Mallocator(const Mallocator<U> &) noexcept {}
-
-    T *allocate(std::size_t n) {
+    Mallocator (const Mallocator<U>& other) noexcept;
+    
+    T *allocate(std::size_t n) 
+	{
         if (n > std::size_t(-1) / sizeof(T))
             throw std::bad_alloc();
-        if (auto p = static_cast<T *>(std::malloc(n * sizeof(T))))
+        if (auto p = static_cast<T*>(std::malloc(n * sizeof(T))))
             return p;
         throw std::bad_alloc();
     }
-
-    void deallocate(T* p, std::size_t) noexcept {
+    
+    void deallocate(T* p, std::size_t) noexcept 
+	{
         std::free(p);
     }
 };
 
-template <class T, class U>
-bool operator==(const Mallocator<T> &, const Mallocator<U> &) {
-    return true;
+template <class T1, class T2>
+bool operator == (const Mallocator<T1>& lhs, const Mallocator<T2>& rhs) 
+{
+	return true;
 }
 
-template <class T, class U>
-bool operator!=(const Mallocator<T> &, const  Mallocator<U> &) {
-    return false;
+template <class T1, class T2>
+bool operator != (const Mallocator<T1>& lhs, const Mallocator<T2> &rhs) {
+	return false;
 }
 
 #endif
