cbf24280fee7bcab3d519e0b9c21950867d5b940
darktulip <darktulip@126.com>
2019-06-20 15:35:36 +0800

    update naive.cpp with c++17 style for comparing

---
+++ naiveallocator.hpp
@@ -0,0 +1,49 @@
+#ifndef NAIVEALLOCATOR_HPP
+#define NAIVEALLOCATOR_HPP
+
+#include <iostream>
+#include <new>
+#include <cstdlib>
+#include <cstddef>
+#include <climits>
+
+template <class T>
+class Mallocator
+{
+	public:
+		typedef T          value_type;
+		typedef size_t     size_type;
+		typedef ptrdiff_t  difference_type;
+
+		Mallocator() = default;
+
+		template <class U>
+		Mallocator (const Mallocator<U>& other) noexcept;
+
+		T *allocate(std::size_t n)
+		{
+			auto buf = (T*)(::operator new((std::size_t)(n * sizeof(T))));
+			if (buf == 0)
+				throw std::bad_alloc();
+			return buf;
+		}
+
+		void deallocate(T* buf, std::size_t)
+		{
+			::operator delete(buf);
+		}
+};
+
+template <class T1, class T2>
+bool operator == (const Mallocator<T1>& lhs, const Mallocator<T2>& rhs)
+{
+	return true;
+}
+
+template <class T1, class T2>
+bool operator != (const Mallocator<T1>& lhs, const Mallocator<T2>& rhs)
+{
+	return false;
+}
+
+#endif
